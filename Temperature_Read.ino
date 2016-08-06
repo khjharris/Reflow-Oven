1f23696714f07ac9e3484ac384daed48a93749e6
Kenwood Harris Jr <kenwood93@gmail.com>
2016-08-05 21:42:33 -0400

    Temperature Logging File

---
+++ Temperature_Read.ino
@@ -0,0 +1,24 @@
+/*This program reads the temperature, and time signature
+of the oven using the Max6675, thermocouple, and SD library
+Kenwood Harris Jr., Rev 1.0, August 5, 2016
+*/
+
+#include <max6675.h>
+#include <RTClib.h>
+#include <SD.h>
+#include <wire.h>
+
+
+void setup() {
+Serial.begin(9600);
+Serial.println("Initiating Temperature logging...")
+
+
+}
+
+void loop() {
+
+
+
+
+}
